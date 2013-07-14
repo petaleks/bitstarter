f4adad88883d4117551f2f5171abb3839f124c21
ALEKSANDAR P <petaleksandar@gmail.com>
2013-07-14 22:45:45 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,18 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var text = fs.readFileSync('index.html','utf8')
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
